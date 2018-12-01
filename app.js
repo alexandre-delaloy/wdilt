243914316dd54a3a162ba946747e632f74f9a06f
ShAdOwInK <alexandredelaloydev@gmail.com>
2018-12-01 01:30:09 +0100

    ♻️ the code is a little bit cleaner

--- app.js
+++ app.js
@@ -1,68 +1,67 @@
 const Calendar = class {
-    constructor(year, month, days, spaceF) {
+    constructor(year, month, days, bfr) {
         this.year = year;
         this.month = month;
         this.days = days;
-        this.spaceF = spaceF
-        this.count = days - 1;
-        this.week = 7;
-        this.weeks = Math.floor((this.spaceF + this.days) / 7 + 1);
-        this.spaceL = this.week * this.weeks - this.spaceF - this.days;
+        this.bfr = bfr
+        this.day = 1;
+        this.weeks = Math.floor((this.bfr + this.days) / 7 + 1);
+        this.ftr = 7 * this.weeks - this.bfr - this.days;
+        this.tmp = [];
         this.space = '                 ';
-        this.weeksObj = [];
-        this.result = `| 🗓 |        Mo       |        Tu       |        We       |        Tu       |        Fr       |        Sa       |        Su       |
+        this.md = `| 🗓 |        Mo       |        Tu       |        We       |        Tu       |        Fr       |        Sa       |        Su       |
 | -- | --------------- | --------------- | --------------- | --------------- | --------------- | --------------- | --------------- |\n`
     }
-    generate() {
-        let weeksArray = [];
+    addDay(index) {
+        return this.tmp[index].push(`[${this.day}](#${this.year}-${this.month}-${this.day})`)
+    }
+    incDay() {
+        if ( this.day <= this.days ) {
+            return this.day++;
+        }
+    }
+    fill() {
         if (this.month < 10) {
             this.month = `0${this.month}`;
         }
         for (let i = 0; i < this.weeks; i++) {
-            weeksArray.push([]);
-            
+            this.tmp.push([]);
         }
-        for (let i = 0; i < this.spaceF; i++) {
-            weeksArray[0].push(this.space);
+        for (let i = 0; i < this.bfr; i++) {
+            this.tmp[0].push(this.space);
         }
-        for (let i = 0; i < this.week - this.spaceF; i++) {
-            let day = this.days - this.count;
-            if (day < 10) {
-                day = `0${day}`;
+        for (let i = 0; i < 7 - this.bfr; i++) {
+            if (this.day < 10) {             
+                this.day = `0${this.day}`;         
             }
-            weeksArray[0].push(`[${day}](#${this.year}-${this.month}-${day})`)
-            if ( this.count >= 0 ) {
-                this.count--;
-            }
+            this.addDay(0);
+            this.incDay();
         }
-        for (let i = 0; i < weeksArray.length; i++) {
-            weeksArray[i].unshift(` W${i+1} `);
-            for (let j = 0; j < this.week; j++) {
-                let day = this.days - this.count;
-                if (day < 10) {
-                    day = `0${day}`;
+        for (let i = 0; i < this.tmp.length; i++) {
+            this.tmp[i].unshift(` W${i+1} `);
+            for (let j = 0; j < 7; j++) {
+                if (this.day < 10) {             
+                    this.day = `0${this.day}`;         
                 }
                 if (i <= this.weeks - 2) {
-                    if (day <=this.days ) {
-                        weeksArray[i+1].push(`[${day}](#${this.year}-${this.month}-${day})`)
+                    if (this.day <=this.days ) {
+                        this.addDay(i + 1);
                     }
                 }
-                
-                if ( this.count >= 0 ) {
-                    this.count--;
-                }
+                this.incDay();
             }   
         }
-        for (let h = 0; h < this.spaceL; h++) {
-            weeksArray[weeksArray.length - 1].push(this.space);
+        for (let h = 0; h < this.ftr; h++) {
+            this.tmp[this.tmp.length - 1].push(this.space);
         }
         for (let i = 0; i < this.weeks; i++) {
-            this.result = this.result + `|${weeksArray[i].join('|')}|\n`;
+            this.md += `|${this.tmp[i].join('|')}|\n`;
         }
-        console.log(this.result);
+        console.log('🛠 | Here we go !\n🏷 | Copy/paste the calendar table in a markdown file. \n==================================================\n');
+        return console.log(this.md);
     }
 }
 const December = new Calendar(2018, 12, 31, 5);
 const January = new Calendar(2019, 01, 31, 1);
 
-new Calendar(2019,04,30,6).generate();
+new Calendar(2018, 12, 31, 5).fill();
